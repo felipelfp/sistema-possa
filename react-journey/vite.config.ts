663c3052381ba090866d931aff2d50790049be2d
Felipe Possa <felipe008lucas@gmail.com>
2026-05-13 22:27:46 -0300

    Transformando em App PWA

--- react-journey/vite.config.ts
+++ react-journey/vite.config.ts
@@ -1,9 +1,36 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+            manifest: {
+                name: 'Sistema Possa',
+                short_name: 'Possa',
+                description: 'Sistema de Controle de Entregas e Finanças',
+                theme_color: '#0A0B1A',
+                background_color: '#0A0B1A',
+                display: 'standalone',
+                icons: [
+                    {
+                        src: 'icon-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'icon-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    }
+                ]
+            }
+        })
+    ],
     base: './', // Use relative paths for assets
     server: {
         host: true, // Listen on all network IP addresses
